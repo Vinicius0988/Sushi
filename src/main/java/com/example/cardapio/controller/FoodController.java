e8919a084a5c951ebf8a9cb305c17640cc4db238
Vinicius_siqueira098 <viniv908@gmail.com>
2025-09-22 23:27:40 -0300

    backend funcionando

---
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,28 @@
+package com.example.cardapio.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodResponseDTO;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository; 
+
+    @GetMapping
+    public List<FoodResponseDTO> getAll() {
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).collect(Collectors.toList());
+        return foodList;
+      
+    }
+}
